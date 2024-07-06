b148cf710eb61c602653b1daa88b5fc8a8445970
Michael Singer <michael@a-singer.de>
2024-07-06 19:21:09 +0200

    initial commit

---
+++ cifconv/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace nlconv
+{
+	class MainClass
+	{
+		public static int Main(string[] args)
+		{
+			return 0;
+		}
+	}
+}
